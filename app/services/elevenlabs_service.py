7a9a0509f86cda3e41e3e24a194fc9109d39a1bc
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-08-13 02:24:11 -0400

    Restructured elevenlabs service to call API at runtime for CI pipeline

--- app/services/elevenlabs_service.py
+++ app/services/elevenlabs_service.py
@@ -2,24 +2,23 @@ import os
 
 from elevenlabs.client import AsyncElevenLabs
 
-api_key = os.getenv("ELEVENLABS_API_KEY")
-voice_id = os.getenv("ELEVENLABS_VOICE_ID")
-
-if not api_key:
-    raise ValueError("ELEVENLABS_API_KEY is not set")
-
-if not voice_id:
-    raise ValueError("ELEVENLABS_VOICE_ID is not set")
-
-client = AsyncElevenLabs(
-    api_key=os.getenv("ELEVENLABS_API_KEY"),
-)
-
 
 async def elevenlabs_tts(response_text: str) -> bytes:
-
     if not response_text:
         raise ValueError("Response from LLM required to generate speech audio")
+
+    api_key = os.getenv("ELEVENLABS_API_KEY")
+    voice_id = os.getenv("ELEVENLABS_VOICE_ID")
+
+    if not api_key:
+        raise ValueError("ELEVENLABS_API_KEY is not set")
+
+    if not voice_id:
+        raise ValueError("ELEVENLABS_VOICE_ID is not set")
+
+    client = AsyncElevenLabs(
+        api_key=api_key,
+    )
 
     audio = client.text_to_speech.convert(
         text=response_text,
